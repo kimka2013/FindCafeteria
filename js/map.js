1aabe1aefe49eae15540fee5a2900b3a85766f52
easycastle <sean34418290@gmail.com>
2021-08-14 20:30:55 +0900

    b

--- js/map.js
+++ js/map.js
@@ -244,7 +244,7 @@ searchPlaces();
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
 
-    var keyword = document.getElementById('keyword').value;
+    var keyword = document.getElementsByClassName('search').value;
 
     if (!keyword.replace(/^\s+|\s+$/g, '')) {
         alert('키워드를 입력해주세요!');
